ec58c9e6cb5ad4581c6bd6b18676687bd2eed175
pawellos@github.com <pablox377@gmail.com>
2019-03-12 20:29:12 +0100

    class for client server

--- chatApp/chatApp/chatApp.cpp
+++ chatApp/chatApp/chatApp.cpp
@@ -3,102 +3,168 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <string>
 #include <WinSock2.h>
 #include <ws2tcpip.h>
-
+#include <sstream>
+#include "clientServer.h"
 #pragma comment(lib, "Ws2_32.lib")
 
 using namespace std;
 
+void Listener_MessageReceived(TCPserver* listener, int client, string msg);
+
 int main()
 {
+	TCPserver server("127.0.0.1", 54000, Listener_MessageReceived);
+
+	if (server.init())
+	{
+		server.run();
+	}
+
+	 
 	/////////////////////// SERVER /////////////////////////////
 	// Initialize Winsock
-	WSADATA wsData;
+	
 
-	int iResult;
-	iResult = WSAStartup(MAKEWORD(2, 2), &wsData);
-	if (iResult != 0) {
-		cerr << "Initialize winsock failed! - Exit" << endl;
-		return 1;
-	}
+	//// Create a socket
+	//SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+	//if (listening == INVALID_SOCKET)
+	//{
+	//	cerr << "Creating socket failed! - Exit" << endl;
+	//	return 1;
+	//}
 
-	// Create a socket
-	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
-	if (listening == INVALID_SOCKET)
-	{
-		cerr << "Creating socket failed! - Exit" << endl;
-		return 1;
-	}
+	//// Bind the socket to an IP address and port 
+	//sockaddr_in hint;
+	//hint.sin_family = AF_INET;
+	//hint.sin_port = htons(54000);
+	//hint.sin_addr.S_un.S_addr = INADDR_ANY;
 
-	// Bind the socket to an IP address and port 
-	sockaddr_in hint;
-	hint.sin_family = AF_INET;
-	hint.sin_port = htons(54000);
-	hint.sin_addr.S_un.S_addr = INADDR_ANY;
-
-	bind(listening, (sockaddr*)&hint, sizeof(hint));
+	//bind(listening, (sockaddr*)&hint, sizeof(hint));
 
 	// Tell Winsock the socket is for listening 
-	listen(listening, SOMAXCONN);
+	//listen(listening, SOMAXCONN);
 
-	// Wait for a connection 
-	sockaddr_in client;
-	int clientSize = sizeof(client);
+	//fd_set master;
+	//FD_ZERO(&master);
+	//FD_SET(listening, &master);
 
-	SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+	//while (true)
+	//{
+	//	fd_set copy = master;
+	//	int socketCount = select(0, &copy, nullptr, nullptr, nullptr);
+	//	for (int i = 0; i < socketCount; i++)
+	//	{
+	//		SOCKET sock = copy.fd_array[i];
+	//		if (sock == listening)
+	//		{
+	//			// Accept a new connection
+	//			SOCKET client = accept(listening, nullptr, nullptr);
+	//			// Add new connection to the list of connected clients
+	//			FD_SET(client, &master);
+	//			// Send a welcone message to the connected client
+	//			string welconeMsg = "Welcome on chat server! \r\n";
+	//			send(client, welconeMsg.c_str(), welconeMsg.size() + 1, 0);
+	//		}
+	//		else
+	//		{
+	//			char buf[4096];
+	//			ZeroMemory(buf, 4096);
 
-	char host[NI_MAXHOST]; // client's remote name
-	char service[NI_MAXSERV]; // Service the client is connect on
-	ZeroMemory(host, NI_MAXHOST);
-	ZeroMemory(service, NI_MAXSERV);
+	//			//Receive message 
+	//			int bytesIn = recv(sock, buf, 4096, 0);
+	//			if (bytesIn <= 0)
+	//			{
+	//				//drop the client 
+	//				closesocket(sock);
+	//				FD_CLR(sock, &master);
 
-	if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
-	{
-		cout << host << "connect on port " << service << endl;
-	}
-	else
-	{
-		inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
-		cout << host << " connected on port " << ntohs(client.sin_port) << endl;
-	}
+	//			}
+	//			else
+	//			{
+	//				// Send message to other clients, and definiaetly NOT listening socket
+	//				for (int i = 0; i < master.fd_count; i++)
+	//				{
+	//					SOCKET outSock = master.fd_array[i];
+	//					if (outSock != listening && outSock != sock)
+	//					{
+	//						ostringstream ss;
+	//						ss << "SOCKET #" << sock << ": " << buf << "\r";
+	//						string strOut = ss.str();
+	//						send(outSock, strOut.c_str(), strOut.size() + 1, 0);
+	//					}
+	//				}
+	//			}
 
-	// Close listening socket 
-	closesocket(listening);
+	//		}
 
-	// While loop: accept and echo message back to client 
-	char buf[4096];
+	//	}
+	//}
 
-	while (true)
-	{
-		ZeroMemory(buf, 4096);
+	//// Wait for a connection 
+	//sockaddr_in client;
+	//int clientSize = sizeof(client);
 
-		//Wait for client to send data
-		int bytesReceived = recv(clientSocket, buf, 4096, 0);
-		if (bytesReceived == SOCKET_ERROR)
-		{
-			cerr << "Error in recv() - Exit" << endl;
-			break;
-		}
-		if (bytesReceived == 0)
-		{
-			cout << "Client Disconnected " << endl;
-			break;
-		}
+	//SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
 
-		//Echo message back to client:
-		send(clientSocket, buf, bytesReceived + 1, 0);
-	}
+	//char host[NI_MAXHOST]; // client's remote name
+	//char service[NI_MAXSERV]; // Service the client is connect on
+	//ZeroMemory(host, NI_MAXHOST);
+	//ZeroMemory(service, NI_MAXSERV);
+
+	//if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
+	//{
+	//	cout << host << "connect on port " << service << endl;
+	//}
+	//else
+	//{
+	//	inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+	//	cout << host << " connected on port " << ntohs(client.sin_port) << endl;
+	//}
+
+	//// Close listening socket 
+	//closesocket(listening);
+
+	//// While loop: accept and echo message back to client 
+	//char buf[4096];
+
+	//while (true)
+	//{
+	//	ZeroMemory(buf, 4096);
+
+	//	//Wait for client to send data
+	//	int bytesReceived = recv(clientSocket, buf, 4096, 0);
+	//	if (bytesReceived == SOCKET_ERROR)
+	//	{
+	//		cerr << "Error in recv() - Exit" << endl;
+	//		break;
+	//	}
+	//	if (bytesReceived == 0)
+	//	{
+	//		cout << "Client Disconnected " << endl;
+	//		break;
+	//	}
+	//	cout << string(buf, 0, bytesReceived) << endl;
+
+	//	//Echo message back to client:
+	//	send(clientSocket, buf, bytesReceived + 1, 0);
+	//}
 
 
 	// Close the socket
-	closesocket(clientSocket);
+	//closesocket(clientSocket);
 
 	// Shutdown winsock
-	WSACleanup();
+	//WSACleanup();
 	/////////////////////////////////////////////////////////
 
 	cin.get();
-    return 0;
+	return 0;
 }
 
+void Listener_MessageReceived(TCPserver * listener, int client, string msg)
+{
+	listener->sendMsg(client, msg);
+}
---
+++ chatApp/chatApp/clientServer.h
@@ -0,0 +1,58 @@
+#pragma once
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <WinSock2.h>
+#include <ws2tcpip.h>
+#include <sstream>
+#pragma comment(lib, "Ws2_32.lib")
+
+constexpr auto MAX_BUFFER_SIZE = 49152;
+
+class TCPserver;
+typedef void(*MessageReceivedHandler)(TCPserver* server, int socketId, std::string msg);
+
+class TCPserver
+{
+private:
+	int m_port;
+	std::string m_ipAddress;
+	MessageReceivedHandler MessageReceived; 
+public:
+	//constructor default
+	TCPserver();
+	//constructor
+	TCPserver(std::string ipAddress, int port, MessageReceivedHandler handler);
+	//Initialize winsock
+	bool init();
+	//Create a winsock 
+	SOCKET createSocket();
+	//Wait for connection
+	SOCKET waitForConnection(SOCKET listening);
+	//Receive loop
+	//The main processing loop
+	void run();
+	//Send back message
+	void sendMsg(int clientSocket, std::string msg);
+	//Clean up
+	void cleanup();
+	
+}; 
+
+
+class TCPclient
+{
+private:
+	int m_port;
+	std::string m_ipAddress;
+
+public:
+	//Initialize winsock
+	bool init();
+	//Create a winsock 
+	SOCKET createSocket();
+	// Connect to server 
+	void connectClientServer();
+	//close down everything
+	void close();
+};
